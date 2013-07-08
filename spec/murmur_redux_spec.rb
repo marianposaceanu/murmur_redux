1afff9a523f297f4bd0e431ac1bf4da644f2e3bd
Marian Posaceanu <contact@marianposaceanu.com>
2013-07-08 21:24:25 +0300

    added specs for main class

---
+++ spec/murmur_redux_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MurmurRedux do
+  let(:string_to_digest) { 'Hello, world!' }
+
+  it 'digests an argument supplied string into a hexadecimal representation' do
+    digested_string = MurmurRedux::Hash.digest(string_to_digest)
+
+    expect(digested_string).should eq('df65d6d2d12d51f164c5f3a85066322c')
+  end
+end
